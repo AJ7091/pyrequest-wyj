a23665b77b460b2f728831bbfd85b04486b4591c
wangyanjiao <wangyanjiao@begoit.com>
2018-01-31 18:20:32 +0800

    add event/guest test case

--- db_fixture/mysql_db.py
+++ db_fixture/mysql_db.py
@@ -1,7 +1,7 @@
 from pymysql import connect,cursors
 from pymysql.err import OperationalError
 import os
-import configparser as cparser # 操作配置文件
+import configparser as cparser # 使用配置文件（.ini）生效
 
 # ===========读取db_config.ini文件设置=============
 base_dir = str(os.path.dirname(os.path.dirname(__file__))) # os.path.dirname(__file__)获取当前文件运行的目录，两次嵌套获取到根目录下
@@ -21,7 +21,7 @@ password = cf.get('mysqlconf','password')
 
 
 # ==============封装Mysql基本操作===============
-class DB:
+class DB():
     def __init__(self):
         try:
             # 连接数据库
@@ -30,7 +30,7 @@ class DB:
                               password=password,
                               db=db,
                               charset='utf8mb4',
-                              cursorclass=cursors.DictCursor) # 查询结果以dic(字典)表示
+                              cursorclass=cursors.DictCursor) # 查询结果以dic(字典)形式返回
         except OperationalError as e:
             print("Mysql Error %d: %s"%(e.args[0],e.args[1]))
 
@@ -39,23 +39,23 @@ class DB:
     def clear(self,table_name):
         #real_sql="truncate table"+table_name+";"
         real_sql="delete from "+table_name+";"
-        with self.conn.cursor() as cursor:   # 游标指针，获得当前指向数据库的指针
-            cursor.execute("SET FOREIGN_KEY_CHECKS=0;")  # execute(query, args):执行单条sql语句。query为sql语句本身，args为参数值的列表。执行后返回值为受影响的行数
+        with self.conn.cursor() as cursor:   # 游标指针，获得当前指向数据库的指针赋值给cursor，也可写成：cursor=self.conn.cursor
+            # execute(query, args):执行单条sql语句。query为sql语句本身，args为参数值的列表。执行后返回值为受影响的行数
+            cursor.execute("SET FOREIGN_KEY_CHECKS=0;")  # 取消外键约束，才好删除表数据
             cursor.execute(real_sql)
         self.conn.commit() # 提交数据库sql操作
 
     # 插入表数据
     def inser(self,table_name,table_data):
         for key in table_data:
-            table_data[key] = "'"+str(table_data[key])+"'"
-        key   = ','.join(table_data.keys())
+            table_data[key] = "'"+str(table_data[key])+"'"  # 改变value的值
+        key   = ','.join(table_data.keys())  # join()方法用于将序列中的元素以指定的字符连接生成一个新的字符串
         value = ','.join(table_data.values())
+
         real_sql = "INSERT INTO " + table_name + " (" + key + ") VALUES (" + value + ")"
-        #print(real_sql)
 
         with self.conn.cursor() as cursor:
             cursor.execute(real_sql)
-
         self.conn.commit()
 
     # 关闭数据库连接
@@ -64,7 +64,7 @@ class DB:
 
     # 初始化测试数据
     def init_data(self,datas):
-        for table,data in datas.items():
+        for table,data in datas.items(): #items()以元组形式返回[(table1,data1),(table2,data2)]
             self.clear(table)
             for d in data:
                 self.inser(table,d)
@@ -75,7 +75,7 @@ class DB:
 if __name__=='__main__':
     db = DB()
     table_name = 'sign_event'
-    data = {'id':1,'name':'红米','`limit`':2000,'status':1,'address':'北京会展中心','start_time':'2016-08-20 00:25:42'}
-    db.clear()
+    data = {'id':1,'name':'红米','`limit`':2000,'status':1,'address':'北京会展中心','start_time':'2016-08-20 00:25:42','create_time':'2000-01-01 00:00:00'}
+    db.clear(table_name)
     db.inser(table_name,data)
     db.close()
---
+++ run_test.py
@@ -0,0 +1,23 @@
+import time,sys
+sys.path.append('./interface_Case')
+sys.path.append('./db_fixture')
+from HTMLTestRunner import HTMLTestRunner
+import unittest
+from db_fixture import test_data
+
+# 指定测试用例为当前文件夹下的interface_Case目录
+test_dir="./interface_Case"
+test_list=unittest.defaultTestLoader.discover(test_dir,pattern='*_test.py')
+
+if __name__=='__main__':
+    test_data.init_data() # 初始化接口测试数据
+
+    now = time.strftime("%Y-%m-%d %H_%M_%S")
+    filename = './report/' + now + '_result.html'
+    fp = open(filename,'wb')
+    runner=HTMLTestRunner(stream=fp,
+                          title='Guest Manage System Interface Test Report',
+                          description='Implementation Example with:',
+                          verbosity=2) # 在窗口打印用例执行结果的详细程度设置
+    runner.run(test_list)
+    fp.close()
